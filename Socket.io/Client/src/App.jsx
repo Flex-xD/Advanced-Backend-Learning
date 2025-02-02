2080763aa368853feb96faf258b0bb8ec62b12b8
Flex-xD <bishtaman88102@gmail.com>
2025-02-02 13:35:40 +0530

    Learned about emit , on andbroadcast event

--- Socket.io/Client/src/App.jsx
+++ Socket.io/Client/src/App.jsx
@@ -2,12 +2,22 @@ import { useEffect } from "react";
 import {io} from "socket.io-client";
 
 const App = () => {
-  const socket = io("http://localhost:3000");
-
+  const socket = io("http://localhost:3000"); //? making a connection with the backend 
   useEffect(() => {
-    socket.on("connect" , () => {
+    socket.on("connect" , () => { // ? connect is a pre build event used to connect to the socket
       console.log("Connected , ID : " , socket.id);
+    });
+
+    socket.on("message" , (payload) => { // ? making a connection to the event that is being sent from the backend !
+      console.log(payload);
     })
+
+    socket.on("broadcastData", (data) => { //? Recieving a broadcast event
+      console.log(data); 
+    })
+
+    socket.emit("welcome" , "First message recievd ! now connected to the backend !"); //? Sending an event "welcome"
+
   } , [])
   return (
     <div>
--- Socket.io/Server/index.js
+++ Socket.io/Server/index.js
@@ -1,30 +1,41 @@
-import express from "express";
+import express, { response } from "express";
 import { Server } from "socket.io";
-import {createServer} from "http";
+import { createServer } from "http";
 import cors from "cors";
 
 const app = express();
 const port = 3000;
 
 const server = createServer(app);
-const io = new Server(server , {
-    cors:{
-        origin:"http://localhost:5173" , 
-        methods:["GET" , "POST"] , 
-        credentials:true
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        methods: ["GET", "POST"],
+        credentials: true
     }
 });
 
 app.use(cors())
 
-app.get("/" ,  (req,  res) => {
+app.get("/", (req, res) => {
     res.send("Hello world !")
 })
 
-io.on("connection" , (Socket) => {
-    console.log(`User connected , UserID : ${Socket.id}`);
+io.on("connection", (socket) => {
+    console.log(`User connected , UserID : ${socket.id}`);
+
+    // * Below I am sending a event from backend to the frontend 
+    socket.emit("message", "This string is the data here !"); // ? Here by using emit , I am sending an event with some data
+
+    // ! Here below by using broadcast I will be sending data to all other socket id's but not to the one which is sending this data
+    socket.broadcast.emit("broadcastData" , "This message is for all of the socket id's out there !");
+
+    // * Here below I am receiving a event from frontend to the backend 
+    socket.on("welcome" , (payload) => {
+        console.log(payload);
+    })
 })
 
-server.listen(port , () => {
+server.listen(port, () => {
     console.log(`Server running on PORT : ${port}`);
 })
